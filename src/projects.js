1c92331b9440f05b53796bbcf5ca2f935cada3c8
Stefan Amariei <as.stefanama@gmail.com>
2021-09-12 20:50:41 +0200

    Started the implementation of the projects page which will contain all my personal projects info + the ability to filter them by tags.

---
+++ src/projects.js
@@ -0,0 +1,30 @@
+export const projects = [
+    {
+        projectName: "Project-1",
+        projectDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non accumsan odio. Duis a odio felis. Aliquam interdum enim felis, eu dignissim neque hendrerit non. In ac consectetur metus. Etiam maximus viverra magna non ornare. Proin a quam id lorem sodales tempor. Quisque a ante id diam tincidunt finibus non vel enim. Sed fermentum feugiat dapibus. Donec et diam neque. Nunc fermentum lacinia vestibulum. Nunc commodo justo neque, sed commodo neque consectetur nec. Duis lobortis, diam sit amet tincidunt gravida, est mi volutpat felis, pellentesque molestie libero lacus ut magna. Aliquam sit amet ante pretium, mattis lorem pulvinar, vulputate dui. Duis non velit vehicula, vehicula justo a, malesuada lorem.",
+        projectTags: "all tag1 tag2",
+        githubLink: "#",
+        liveSiteLink: "#"
+    },
+    {
+        projectName: "Project-2",
+        projectDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non accumsan odio. Duis a odio felis. Aliquam interdum enim felis, eu dignissim neque hendrerit non. In ac consectetur metus. Etiam maximus viverra magna non ornare. Proin a quam id lorem sodales tempor. Quisque a ante id diam tincidunt finibus non vel enim. Sed fermentum feugiat dapibus. Donec et diam neque. Nunc fermentum lacinia vestibulum. Nunc commodo justo neque, sed commodo neque consectetur nec. Duis lobortis, diam sit amet tincidunt gravida, est mi volutpat felis, pellentesque molestie libero lacus ut magna. Aliquam sit amet ante pretium, mattis lorem pulvinar, vulputate dui. Duis non velit vehicula, vehicula justo a, malesuada lorem.",
+        projectTags: "all tag1",
+        githubLink: "#",
+        liveSiteLink: "#"
+    },
+    {
+        projectName: "Project-3",
+        projectDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non accumsan odio. Duis a odio felis. Aliquam interdum enim felis, eu dignissim neque hendrerit non. In ac consectetur metus. Etiam maximus viverra magna non ornare. Proin a quam id lorem sodales tempor. Quisque a ante id diam tincidunt finibus non vel enim. Sed fermentum feugiat dapibus. Donec et diam neque. Nunc fermentum lacinia vestibulum. Nunc commodo justo neque, sed commodo neque consectetur nec. Duis lobortis, diam sit amet tincidunt gravida, est mi volutpat felis, pellentesque molestie libero lacus ut magna. Aliquam sit amet ante pretium, mattis lorem pulvinar, vulputate dui. Duis non velit vehicula, vehicula justo a, malesuada lorem.",
+        projectTags: "all tag2",
+        githubLink: "#",
+        liveSiteLink: "#"
+    },
+    {
+        projectName: "Project-4",
+        projectDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non accumsan odio. Duis a odio felis. Aliquam interdum enim felis, eu dignissim neque hendrerit non. In ac consectetur metus. Etiam maximus viverra magna non ornare. Proin a quam id lorem sodales tempor. Quisque a ante id diam tincidunt finibus non vel enim. Sed fermentum feugiat dapibus. Donec et diam neque. Nunc fermentum lacinia vestibulum. Nunc commodo justo neque, sed commodo neque consectetur nec. Duis lobortis, diam sit amet tincidunt gravida, est mi volutpat felis, pellentesque molestie libero lacus ut magna. Aliquam sit amet ante pretium, mattis lorem pulvinar, vulputate dui. Duis non velit vehicula, vehicula justo a, malesuada lorem.",
+        projectTags: "all tag3",
+        githubLink: "#",
+        liveSiteLink: "#"
+    },
+]
---
+++ src/projectsPage.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react"; 
+import { projects } from "./projects";
+
+const ProjectsPage = () => {
+
+}
+
+const ProjectsMenu = () => {
+
+}
+
+const ProjectsBody = () => {
+
+}
+
+const CotactMe = () => {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default ProjectsPage;
